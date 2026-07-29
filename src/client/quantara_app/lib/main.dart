7b4c6c17abd9b2b25b5167c76eb985859cd3c72c
Hamidreza Pakpour <136631329+Hamrez95@users.noreply.github.com>
2026-07-29 16:07:23 +0330

    Defer background bootstrap until after first frame

--- src/client/quantara_app/lib/main.dart
+++ src/client/quantara_app/lib/main.dart
@@ -1,8 +1,15 @@
+import 'dart:async';
+
 import 'package:flutter/widgets.dart';
 
 import 'app/quantara_app.dart';
+import 'features/owner_alpha/data/background_opportunity_scanner.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const QuantaraApp());
+
+  WidgetsBinding.instance.addPostFrameCallback((_) {
+    unawaited(BackgroundOpportunityScanner.initialize());
+  });
 }
