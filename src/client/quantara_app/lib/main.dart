f018ccea7b4e18829c8d469a78578b0c27290985
Hamidreza Pakpour <136631329+Hamrez95@users.noreply.github.com>
2026-07-30 23:39:05 +0330

    feat: initialize local live foreground communication

--- src/client/quantara_app/lib/main.dart
+++ src/client/quantara_app/lib/main.dart
@@ -1,12 +1,14 @@
 import 'dart:async';
 
 import 'package:flutter/widgets.dart';
+import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 
 import 'app/quantara_app.dart';
 import 'features/owner_alpha/data/background_opportunity_scanner.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  FlutterForegroundTask.initCommunicationPort();
   runApp(const QuantaraApp());
 
   WidgetsBinding.instance.addPostFrameCallback((_) {
