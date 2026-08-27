f543a9468f0ef12483ba9635970963e7e09d1230
Hamidreza Pakpour <136631329+Hamrez95@users.noreply.github.com>
2026-08-27 03:58:57 +0330

    refactor(windows): isolate network monitor from IPC listener

--- src/windows_service/ipc_readonly_listener.cpp
+++ src/windows_service/ipc_readonly_listener.cpp
@@ -4,7 +4,6 @@
 
 #include "ipc_readonly_session.h"
 #include "local_pipe_security.h"
-#include "network_change_monitor.h"
 
 namespace quantara {
 namespace {
@@ -39,18 +38,13 @@ bool WaitForClientOrStop(HANDLE pipe, HANDLE stop_event) noexcept {
 
 bool RunReadOnlyStatusListener(
     HANDLE stop_event, const std::wstring& pipe_name,
-    std::atomic<ServiceSafetyState>& safety_state) noexcept {
+    const std::atomic<ServiceSafetyState>& safety_state) noexcept {
   if (stop_event == nullptr || stop_event == INVALID_HANDLE_VALUE ||
       pipe_name.empty()) {
     return false;
   }
 
   try {
-    NetworkChangeMonitor network_monitor(safety_state);
-    if (!network_monitor.Start()) {
-      return false;
-    }
-
     RequestReplayGuard replay_guard;
     while (WaitForSingleObject(stop_event, 0) == WAIT_TIMEOUT) {
       HANDLE pipe = CreateLocalPipeServer(pipe_name);
