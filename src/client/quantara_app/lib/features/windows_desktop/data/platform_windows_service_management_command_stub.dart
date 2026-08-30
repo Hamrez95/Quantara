b44a1d0acd5635a24b3279102428b89de6ec24c7
Hamidreza Pakpour <136631329+Hamrez95@users.noreply.github.com>
2026-08-30 14:04:44 +0330

    feat(windows): add non-Windows management stub

---
+++ src/client/quantara_app/lib/features/windows_desktop/data/platform_windows_service_management_command_stub.dart
@@ -0,0 +1,8 @@
+import '../application/windows_service_management_client.dart';
+
+WindowsServiceCloseExistingPositionCommand
+createWindowsServiceCloseExistingPositionCommand() {
+  return (positionId) async => throw const WindowsServiceManagementException(
+        'Windows service management is unavailable on this platform.',
+      );
+}
