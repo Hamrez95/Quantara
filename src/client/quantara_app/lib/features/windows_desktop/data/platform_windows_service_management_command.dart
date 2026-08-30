d38cf3a34edabd3c162370c6412c4cea1c2df756
Hamidreza Pakpour <136631329+Hamrez95@users.noreply.github.com>
2026-08-30 14:04:38 +0330

    feat(windows): wire platform management client factory

---
+++ src/client/quantara_app/lib/features/windows_desktop/data/platform_windows_service_management_command.dart
@@ -0,0 +1,10 @@
+import '../application/windows_service_management_client.dart';
+import 'platform_windows_service_management_command_stub.dart'
+    if (dart.library.io) 'platform_windows_service_management_command_io.dart'
+    as platform;
+
+WindowsServiceManagementClient createPlatformWindowsServiceManagementClient() {
+  return WindowsServiceManagementClient(
+    closeCommand: platform.createWindowsServiceCloseExistingPositionCommand(),
+  );
+}
