85a77b18ef430f9aaa6ee42a3fe8babe703a46c5
Hamidreza Pakpour <136631329+Hamrez95@users.noreply.github.com>
2026-09-01 08:44:45 +0330

    feat(AI): coordinate real Supervisor support sessions

--- src/client/quantara_app/lib/features/ai_supervisor/application/supervisor_connection_controller.dart
+++ src/client/quantara_app/lib/features/ai_supervisor/application/supervisor_connection_controller.dart
@@ -3,27 +3,32 @@ import 'package:flutter/foundation.dart';
 import '../domain/supervisor_connection.dart';
 import 'supervisor_connection_health_coordinator.dart';
 import 'supervisor_secure_setup_store.dart';
+import 'supervisor_support_session_client.dart';
 
 final class SupervisorConnectionController extends ChangeNotifier {
   factory SupervisorConnectionController({
     required SupervisorSecureSetupStore setupStore,
     required SupervisorConnectionHealthCoordinator healthCoordinator,
     required bool releaseBuild,
+    SupervisorSupportSessionClient? supportSessionClient,
   }) => SupervisorConnectionController._(
     setupStore,
     healthCoordinator,
     releaseBuild,
+    supportSessionClient ?? SupervisorSupportSessionClient(setupStore: setupStore),
   );
 
   SupervisorConnectionController._(
     this._setupStore,
     this._healthCoordinator,
     this._releaseBuild,
+    this._supportSessionClient,
   );
 
   final SupervisorSecureSetupStore _setupStore;
   final SupervisorConnectionHealthCoordinator _healthCoordinator;
   final bool _releaseBuild;
+  final SupervisorSupportSessionClient _supportSessionClient;
 
   SupervisorConnectionSnapshot _snapshot =
       SupervisorConnectionSnapshot.notConfigured();
@@ -71,8 +76,21 @@ final class SupervisorConnectionController extends ChangeNotifier {
     await _checkConfigured(setup.serverOrigin, revision: revision);
   }
 
+  Future<bool> startSupportSession(Duration duration) {
+    return _supportSessionClient.start(
+      connection: _snapshot,
+      duration: duration,
+      releaseBuild: _releaseBuild,
+    );
+  }
+
+  Future<void> stopSupportSession() {
+    return _supportSessionClient.stop(connection: _snapshot);
+  }
+
   Future<void> clear() async {
     ++_revision;
+    await stopSupportSession();
     await _setupStore.clear();
     _setSnapshot(SupervisorConnectionSnapshot.notConfigured());
   }
